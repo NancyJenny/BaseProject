877888ccaecab1d1ee9a6444b96e2a2f85290586
jenny <dadada26305@gmail.com>
2017-12-06 12:04:47 +0800

    第一版

---
+++ src/main/java/com/github/BancyJenny/Student.java
@@ -0,0 +1,17 @@
+package com.github.BancyJenny;
+
+public class Student {
+
+    private int id;
+    public  int StudentId(int id)
+    {
+        if(id > 10)
+        {
+            return 0;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+}
---
+++ src/test/java/com/github/BancyJenny/StudentTest.java
@@ -0,0 +1,13 @@
+package com.github.BancyJenny;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StudentTest {
+    @Test
+    public void studentId() throws Exception {
+
+    }
+
+}
