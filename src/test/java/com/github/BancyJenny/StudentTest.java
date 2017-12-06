e7559229324983ddacc4d4a7e9614edf0c4c6c4a
jenny <dadada26305@gmail.com>
2017-12-06 12:21:54 +0800

    第一版.

--- src/test/java/com/github/BancyJenny/StudentTest.java
+++ src/test/java/com/github/BancyJenny/StudentTest.java
@@ -7,7 +7,9 @@ import static org.junit.Assert.*;
 public class StudentTest {
     @Test
     public void studentId() throws Exception {
-
+    Student s = new Student();
+        int y = s.StudentId(20);
+        System.out.print(y);
     }
 
 }
